c4b45aec8dc96e212437812bde27740ac4bb9bf5
sgawarat <sgawarat@gmail.com>
2025-11-18 22:04:02 +0900

    Update npm packages

--- libs/remark-obsidian-wikilink/src/syntax.ts
+++ libs/remark-obsidian-wikilink/src/syntax.ts
@@ -1,6 +1,12 @@
 // SPDX-License-Identifier: MIT
 import { codes } from "micromark-util-symbol";
-import type { Code, Effects, Extension, State } from "micromark-util-types";
+import type {
+  Code,
+  Effects,
+  Extension,
+  State,
+  TokenizeContext,
+} from "micromark-util-types";
 
 declare module "micromark-util-types" {
   interface TokenTypeMap {
@@ -13,149 +19,156 @@ declare module "micromark-util-types" {
   }
 }
 
+const EMBEDDED_PREFIX = codes.exclamationMark;
+const OPENING = codes.leftSquareBracket;
+const TEXT_PREFIX = codes.verticalBar;
+const CLOSING = codes.rightSquareBracket;
+
 function isBreak(code: Code): code is null {
   return code === null || code <= codes.carriageReturnLineFeed;
 }
 
-class TokenizerBuilder {
-  static readonly ESCAPING = codes.backslash;
-  static readonly EMBEDDED = codes.exclamationMark;
-  static readonly OPENING = codes.leftSquareBracket;
-  static readonly TEXT_PREFIX = codes.verticalBar;
-  static readonly CLOSING = codes.rightSquareBracket;
+function tokenizeBody(
+  this: TokenizeContext,
+  effects: Effects,
+  ok: State,
+  nok: State,
+): State {
+  return startPath;
 
-  build(effects: Effects, ok: State, nok: State): State {
-    const self = this;
-    return prestart;
-
-    function prestart(code: Code): State | undefined {
-      return self.start(code, effects, ok, nok);
+  function startPath(code: Code): State | undefined {
+    if (isBreak(code)) return nok(code);
+    if (code === TEXT_PREFIX) {
+      return prestartText(code);
     }
-  }
-
-  private start(
-    code: Code,
-    effects: Effects,
-    ok: State,
-    nok: State,
-  ): State | undefined {
-    const self = this;
-
-    if (code !== TokenizerBuilder.OPENING) return nok(code);
-    effects.enter("obsidianWikilink");
-    effects.enter("obsidianWikilinkOpen");
-    effects.consume(code);
-    return open;
-
-    function open(code: Code): State | undefined {
-      if (code !== TokenizerBuilder.OPENING) return nok(code);
-      effects.consume(code);
-      effects.exit("obsidianWikilinkOpen");
-      return self.body(effects, ok, nok);
-    }
-  }
-
-  private body(effects: Effects, ok: State, nok: State): State {
-    const self = this;
-    return startPath;
-
-    function startPath(code: Code): State | undefined {
-      if (isBreak(code)) return nok(code);
-      if (code === TokenizerBuilder.TEXT_PREFIX) return prestartText(code);
-      if (code === TokenizerBuilder.CLOSING) {
-        return self.close(code, effects, ok, nok);
-      }
-      effects.enter("obsidianWikilinkPath");
-      effects.consume(code);
-      return finishPath;
-    }
-
-    function finishPath(code: Code): State | undefined {
-      if (isBreak(code)) return nok(code);
-      if (code === TokenizerBuilder.TEXT_PREFIX) {
-        effects.exit("obsidianWikilinkPath");
-        return prestartText(code);
-      }
-      if (code === TokenizerBuilder.CLOSING) {
-        effects.exit("obsidianWikilinkPath");
-        return self.close(code, effects, ok, nok);
-      }
-      effects.consume(code);
-      return finishPath;
-    }
-
-    function prestartText(code: Code): State | undefined {
-      if (code !== TokenizerBuilder.TEXT_PREFIX) return nok(code);
-      effects.enter("obsidianWikilinkTextPrefix");
-      effects.consume(code);
-      effects.exit("obsidianWikilinkTextPrefix");
-      return startText;
-    }
-
-    function startText(code: Code): State | undefined {
-      if (isBreak(code)) return nok(code);
-      if (code === TokenizerBuilder.CLOSING) {
-        return self.close(code, effects, ok, nok);
-      }
-      effects.enter("obsidianWikilinkText");
-      effects.enter("chunkText", { contentType: "text" });
-      effects.consume(code);
-      return finishText;
-    }
-
-    function finishText(code: Code): State | undefined {
-      if (isBreak(code)) return nok(code);
-      if (code === TokenizerBuilder.CLOSING) {
-        effects.exit("chunkText");
-        effects.exit("obsidianWikilinkText");
-        return self.close(code, effects, ok, nok);
-      }
-      effects.consume(code);
-      return finishText;
-    }
-  }
-
-  private close(
-    code: Code,
-    effects: Effects,
-    ok: State,
-    nok: State,
-  ): State | undefined {
-    if (code !== TokenizerBuilder.CLOSING) return nok(code);
-    effects.enter("obsidianWikilinkClose");
-    effects.consume(code);
-    return finish;
-
-    function finish(code: Code): State | undefined {
-      if (code !== TokenizerBuilder.CLOSING) return nok(code);
-      effects.consume(code);
-      effects.exit("obsidianWikilinkClose");
-      effects.exit("obsidianWikilink");
+    if (code === CLOSING) {
       return ok(code);
     }
+    effects.enter("obsidianWikilinkPath");
+    effects.consume(code);
+    return finishPath;
+  }
+
+  function finishPath(code: Code): State | undefined {
+    if (isBreak(code)) return nok(code);
+    if (code === TEXT_PREFIX) {
+      effects.exit("obsidianWikilinkPath");
+      return prestartText(code);
+    }
+    if (code === CLOSING) {
+      effects.exit("obsidianWikilinkPath");
+      return ok(code);
+    }
+    effects.consume(code);
+    return finishPath;
+  }
+
+  function prestartText(code: Code): State | undefined {
+    if (code !== TEXT_PREFIX) return nok(code);
+    effects.enter("obsidianWikilinkTextPrefix");
+    effects.consume(code);
+    effects.exit("obsidianWikilinkTextPrefix");
+    return startText;
+  }
+
+  function startText(code: Code): State | undefined {
+    if (isBreak(code)) return nok(code);
+    if (code === CLOSING) {
+      return ok(code);
+    }
+    effects.enter("obsidianWikilinkText");
+    effects.enter("chunkText", { contentType: "text" });
+    effects.consume(code);
+    return finishText;
+  }
+
+  function finishText(code: Code): State | undefined {
+    if (isBreak(code)) return nok(code);
+    if (code === CLOSING) {
+      effects.exit("chunkText");
+      effects.exit("obsidianWikilinkText");
+      return ok(code);
+    }
+    effects.consume(code);
+    return finishText;
+  }
+}
+
+function tokenizeBodyWithBrackets(
+  this: TokenizeContext,
+  effects: Effects,
+  ok: State,
+  nok: State,
+): State {
+  const body = tokenizeBody.call(this, effects, close, nok);
+  return open;
+
+  function open(code: Code): State | undefined {
+    if (code !== OPENING) return nok(code);
+    effects.enter("obsidianWikilinkOpen");
+    effects.consume(code);
+    return (code: Code): State | undefined => {
+      if (code !== OPENING) return nok(code);
+      effects.consume(code);
+      effects.exit("obsidianWikilinkOpen");
+      return body;
+    };
+  }
+
+  function close(code: Code): State | undefined {
+    if (code !== CLOSING) return nok(code);
+    effects.enter("obsidianWikilinkClose");
+    effects.consume(code);
+    return (code: Code): State | undefined => {
+      if (code !== CLOSING) return nok(code);
+      effects.consume(code);
+      effects.exit("obsidianWikilinkClose");
+      return ok(code);
+    };
+  }
+}
+
+function tokenizeNormalLink(
+  this: TokenizeContext,
+  effects: Effects,
+  ok: State,
+  nok: State,
+): State {
+  const bodyWithBrackets = tokenizeBodyWithBrackets.call(
+    this,
+    effects,
+    exit,
+    nok,
+  );
+  return enter;
+
+  function enter(code: Code): State | undefined {
+    if (code !== OPENING) return nok(code);
+    effects.enter("obsidianWikilink");
+    return bodyWithBrackets(code);
+  }
+
+  function exit(code: Code): State | undefined {
+    if (code !== CLOSING) return nok(code);
+    effects.exit("obsidianWikilink");
+    return ok(code);
   }
 }
 
 export function obsidianWikilinkSyntax(): Extension {
-  const builder = new TokenizerBuilder();
   return {
     text: {
-      [TokenizerBuilder.OPENING]: {
+      [OPENING]: {
         name: "obsidianWikilink",
         previous(code) {
-          return (
-            code !== TokenizerBuilder.ESCAPING &&
-            code !== TokenizerBuilder.EMBEDDED
-          );
+          return code !== EMBEDDED_PREFIX;
         },
         tokenize(effects, ok, nok) {
           // 失敗時に余分なトークンを残さないようにattemptでトークン化する
           return effects.attempt(
             {
               name: "obsidianWikilinkAttempt",
-              tokenize(effects, ok, nok) {
-                return builder.build(effects, ok, nok);
-              },
+              tokenize: tokenizeNormalLink,
             },
             ok,
             nok,
